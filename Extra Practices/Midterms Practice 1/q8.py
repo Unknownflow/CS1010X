3f8a3a491ce5313acef42eb5a29263f86f5bdfa9
Unknownflow <clarencelau88@gmail.com>
2025-04-07 21:47:06 +0800

    fin midterms prac 1 q8

--- Extra Practices/Midterms Practice 1/q8.py
+++ Extra Practices/Midterms Practice 1/q8.py
@@ -6,27 +6,17 @@ def count_change(amount, kind_of_coins):
         if amount < 0: 
             return 0
         if kind_of_coins < 0:
-            return 1
+            return 0
         else:
-            #print(kind_of_coins)\
-
-            if kind_of_coins - 1 < -1:
-                return count_change(amount, -1)
             change = changeArr[kind_of_coins]
-            print(change)
+
             if change > amount:
-                return count_change(amount, kind_of_coins-1)
+                return helper(amount, kind_of_coins-1)
             else:
-                return count_change(amount, kind_of_coins-1) + count_change(amount-change, kind_of_coins)
+                return helper(amount, kind_of_coins-1) + helper(amount-change, kind_of_coins)
 
     return helper(amount, kind_of_coins-1)
 
-
-#print(count_change(1,5))
-
-#print(count_change(5,5))
-#print(count_change(10, 5))
-#print(count_change(20, 5))
-
-
 print(count_change(5,2))
+print(count_change(10, 5))
+print(count_change(20, 5))
