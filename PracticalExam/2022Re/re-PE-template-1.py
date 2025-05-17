702394f847eed3f6dbcc906ac9b32a9b5864e7fc
Unknownflow <clarencelau88@gmail.com>
2025-05-17 16:14:43 +0800

    wip: 2022re

--- PracticalExam/2022Re/re-PE-template-1.py
+++ PracticalExam/2022Re/re-PE-template-1.py
@@ -8,24 +8,52 @@ plain = [ ['0','1','2','3','4','5'],
 
 message = "HELLO 1 2 3"
 def encrypt(message, plain, c1, c2): #'012345', 'ABCDEF'):
-    pass
+    res = ""
+    for char in message:
+        if char == " ":
+            res += char
+        else:
+            for i in range(len(plain)):
+                for j in range(len(plain[0])):
+                    if plain[i][j] == char:
+                        res += c1[i]
+                        res += c2[j]
+                        break
+
+    return res
+            
 
 secret = encrypt(message, plain, '062849', 'abcdef')
-print(secret)
+#print(secret)
 
 # Q2
 def decrypt(secret, plain, c1, c2):
-    pass
+    res = ""
+    curr_ptr = 0
+    length = len(secret)
+    while curr_ptr < length:
+        if secret[curr_ptr] == " ":
+            res += " "
+            curr_ptr += 1
+        else:
+            row_idx = c1.index(secret[curr_ptr])
+            col_idx = c2.index(secret[curr_ptr + 1])
+            res += plain[row_idx][col_idx]
+            curr_ptr += 2
 
-print( decrypt(secret, plain, '062849', 'abcdef'))
+    return res
+
+#print( decrypt(secret, plain, '062849', 'abcdef'))
 
 # Q3
 def index(lst, char):
     pass
+        
+        
 
 lst = [0,[1,2,3,4],[5,6,[7,[8,9]]]]
-for i in range(12):
-    print("index of", i, "in", lst, "is:", index(lst, i))
+#for i in range(12):
+#    print("index of", i, "in", lst, "is:", index(lst, i))
 
 # Q4 - you are not allowed to use any library functions
 def print_matrix(matrix):
@@ -33,32 +61,120 @@ def print_matrix(matrix):
         print (matrix[i])
         
 def make_rotation_matrix(m, n):
-    pass
+    matrix = []
+    row = [i for i in range(0, n)]
+    for i in range(m):
+        matrix.append(row)
+        row = list(row)
+        popped = row.pop(0)
+        row.append(popped)
 
-print("rotating")
+    return matrix
+
+#print("rotating")
 #print_matrix(make_rotation_matrix(5, 5))
+#print()
+#print_matrix(make_rotation_matrix(6, 5))
 
 # Q5
 def make_symmetrical_matrix(n):
-    pass
+    matrix = [[0 for i in range(n)] for j in range(n)]
+    for i in range(n):
+        matrix[0][i] = i
+        matrix[i][0] = i
+
+    for i in range(1,n):
+        for j in range(1, n):
+            matrix[i][j] = matrix[i-1][j-1]
+    return matrix
 
 print("symmetric")
 #print_matrix(make_symmetrical_matrix(6))
 
 # Q6
 def make_concentric_matrix(m,n):
-    pass
+    matrix = [[0 for i in range(n)] for j in range(m)]
+    curr_num = 1
+    left_ptr = 1
+    top_ptr = 1
+    right_ptr = n-2
+    bottom_ptr = m-2
+    while left_ptr <= right_ptr and top_ptr <= bottom_ptr:
+        for i in range(left_ptr, right_ptr + 1):
+            matrix[top_ptr][i] = curr_num
+            matrix[bottom_ptr][i] = curr_num
+
+        for i in range(top_ptr, bottom_ptr + 1):
+            matrix[i][left_ptr] = curr_num
+            matrix[i][right_ptr] = curr_num
+
+        curr_num += 1
+        left_ptr += 1
+        right_ptr -= 1
+        top_ptr += 1
+        bottom_ptr -= 1
+    
+    return matrix
 
 print("concentric")
+#print_matrix(make_concentric_matrix(3,3))
+#print_matrix(make_concentric_matrix(4,4))
+#print_matrix(make_concentric_matrix(5,5))
+#print_matrix(make_concentric_matrix(5,6))
+
 #print_matrix(make_concentric_matrix(5,10))
 
 # Q7
 def make_diamond_matrix(m,n):
-    pass
+    matrix = [[0 for i in range(n)] for j in range(m)]
+    if m % 2 == 0:
+        if n % 2 == 0:
+            for i in range(m // 2):
+                for j in range(n // 2):
+                    matrix[i][j] = j + i
+                    matrix[i][n-j-1] = j + i
+
+        else:
+            for i in range(m // 2):
+                for j in range(n // 2 + 1):
+                    matrix[i][j] = j + i
+                    matrix[i][n-j-1] = j + i
+        diff = 1
+        for i in range(m // 2, m):
+            for j in range(n):
+                matrix[i][j] = matrix[i-diff][j]
+            diff += 2                    
+    else:
+        if n % 2 == 0:
+            for i in range(m // 2 + 1):
+                for j in range(n // 2):
+                    matrix[i][j] = j + i
+                    matrix[i][n-j-1] = j + i
+
+        else:
+            for i in range(m // 2 + 1):
+                for j in range(n // 2 + 1):
+                    matrix[i][j] = j + i
+                    matrix[i][n-j-1] = j + i
+                    
+
+        diff = 2
+        for i in range(m // 2 + 1, m):
+            for j in range(n):
+                matrix[i][j] = matrix[i-diff][j]
+            diff += 2      
+    
+
+    return matrix
 
 print("diamond")
-#print_matrix(make_diamond_matrix(7,7))
-
+print_matrix(make_diamond_matrix(7,7))
+print()
+#print_matrix(make_diamond_matrix(8,8))
+print()
+print_matrix(make_diamond_matrix(7,8))
+print()
+#print_matrix(make_diamond_matrix(8,7))
 
 # Q8 - Q10
 class Node(object):
@@ -95,7 +211,17 @@ def insert_into(head, num):
 
 # Q8
 def all_keys(node):
-    pass
+    curr = node
+    res = ""
+
+    while curr != None:
+        res += str(curr.num) + "-"
+        curr = curr.after
+
+    if len(res) > 0 and res[-1] == "-":
+        return res[:-1]
+    else:
+        return res
 
 lst = None
 for i in [4,3,16,14,24,2,5,22,17,9,1,6,11,7,18,12,13]:
@@ -103,11 +229,24 @@ for i in [4,3,16,14,24,2,5,22,17,9,1,6,11,7,18,12,13]:
 
 # Q9
 def search_layer(node, num):
-    pass
+    curr = node
+    res = ""
+    found = False
 
-print("Current full list is:", all_keys(lst))
-for i in range(1,26,5):
-    print("visited nodes in searching for", i, ":", search_layer(lst, i))
+    while curr != None and not found:
+        if curr.num >= num:
+            found = True
+        res += str(curr.num) + "-"
+        curr = curr.after
+
+    if len(res) > 0 and res[-1] == "-":
+        return res[:-1]
+    else:
+        return res
+
+#print("Current full list is:", all_keys(lst))
+#for i in range(1,27,5):
+    #print("visited nodes in searching for", i, ":", search_layer(lst, i))
 
 # Q10
 import random
@@ -135,15 +274,55 @@ topMostLst = create_top(topLst)
 print("Current topMostLst:", all_keys(topMostLst))
 
 def search(topMostLst, num):
-    pass
+    curr = topMostLst
+    found = False
+    res = ""
+    
+    while curr != None and not found:
+        if curr.num == num:
+            found = True
+            res += str(curr.num)
+            break
+        elif curr.num > num:
+            res += str(curr.num) + "-"
+            if curr:
+                curr = curr.before
+                if curr:
+                    curr = curr.bottom
+                    if curr:
+                        curr = curr.after 
+                else:
+                    break
+            continue
+
+        res += str(curr.num) + "-"
+        if curr.after:
+            if curr.after.num > num:
+                curr = curr.bottom
+                if curr:
+                    curr = curr.after
+            else:
+                curr = curr.after
+        else:
+            curr = curr.bottom
+            if curr:
+                curr = curr.after
+
+
+
+    if len(res) > 0 and res[-1] == "-":
+        return res[:-1]
+    else:
+        return res
 
 for i in range(25):
     print("searching using 1 list", i, ":", search(lst, i))    
 
-for i in range(25):
+for i in range(26):
     print("searching using 2 lists", i, ":", search(topLst, i))    
 
-for i in range(25):
+print()
+for i in range(26):
    print("searching using 3 lists", i, ":", search(topMostLst, i))
 
 
